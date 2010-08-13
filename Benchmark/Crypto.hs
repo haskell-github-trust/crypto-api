91fe7d9356f3725f64ea263acb3aa80f2751d297
thomas.dubuisson <thomas.dubuisson@gmail.com>
2010-08-13 07:31:35 -0700

    Add cipher benchmark
    Ignore-this: f518a4d4d4d1aa328fe591d7cb8a2305
    
    darcs-hash:20100813143135-cef97-95dc9fb058eeb92734b0844286b26626ab1bf5c3.gz

--- Benchmark/Crypto.hs
+++ Benchmark/Crypto.hs
@@ -1,3 +1,7 @@
+module Benchmark.Crypto
+	( benchmarkHash
+	, benchmarkBlockCipher
+	) where
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import Data.Serialize as Ser
@@ -6,6 +10,7 @@ import Criterion
 import Criterion.Config
 import qualified Criterion.MultiMap as M
 
+-- 128KB strings
 ps = B.replicate (2^27) 0
 lps = L.replicate (2^27) 0
 
@@ -17,3 +22,9 @@ benchHash h name = do
 
 op :: Ser.Serizalize d => (a -> d) -> a -> Pure
 op f str = whnf (B.unpack . Ser.encode . f) str
+
+benchmarkBlockCipher :: BlockCipher k => k -> String -> IO ()
+benchmarkBlockCipher k name =
+	let benchs = bgroup 	[ bench (name ++ "-enc") (ecb k) ps
+				, bench (name ++ "-dec") (ecb k) ps]
+	run benchs 1000
