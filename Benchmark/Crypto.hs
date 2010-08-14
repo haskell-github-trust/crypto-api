d8adfea274328252e5a204af5f4411123f733434
thomas.dubuisson <thomas.dubuisson@gmail.com>
2010-08-13 18:39:25 -0700

    Fix cabal to build Benchmark.Crypto, fix benchmarks
    Ignore-this: 4852db0b36e497307e581cd4f5399f59
    
    darcs-hash:20100814013925-cef97-3a137aa552bbe86aa35c768974c1152614c17cc6.gz

--- Benchmark/Crypto.hs
+++ Benchmark/Crypto.hs
@@ -2,28 +2,34 @@ module Benchmark.Crypto
 	( benchmarkHash
 	, benchmarkBlockCipher
 	) where
+
+import Data.Crypto.Classes
+import Data.Crypto.Modes (ecb', unEcb')
+import qualified Data.Serialize as Ser
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import Data.Serialize as Ser
 import Criterion
-import Criterion.Config
-import qualified Criterion.MultiMap as M
+import Control.Monad (liftM)
 
 -- 128KB strings
 ps = B.replicate (2^27) 0
 lps = L.replicate (2^27) 0
 
-benchHash :: Hash c d => d => String -> IO ()
-benchHash h name = do
-        let benchs = bgroup	[ bench (name ++ "-lazy")   (op (hashFunc h) lps)
-				, bench (name ++ "-strict") (op (hashFunc' h) ps)]
-	run benchs 1000
+benchmarkHash :: Hash c d => d -> String -> Benchmark
+benchmarkHash h name =
+        let benchs = bgroup name [ bench "lazy"   (whnf (hashFunc h) lps)
+				 , bench "strict" (whnf (hashFunc' h) ps)] :: Benchmark
+	in benchs
 
-op :: Ser.Serizalize d => (a -> d) -> a -> Pure
+op :: Ser.Serialize d => (a -> d) -> a -> Pure
 op f str = whnf (B.unpack . Ser.encode . f) str
 
-benchmarkBlockCipher :: BlockCipher k => k -> String -> IO ()
+benchmarkBlockCipher :: BlockCipher k => k -> String -> Benchmark
 benchmarkBlockCipher k name =
-	let benchs = bgroup 	[ bench (name ++ "-enc") (ecb k) ps
-				, bench (name ++ "-dec") (ecb k) ps]
-	run benchs 1000
+	let benchs = bgroup name [ bench "enc" (whnf (ecb' k) ps)
+				 , bench "dec" (whnf (unEcb' k) ps)] :: Benchmark
+	in benchs
+
+benchmarkRNG :: (Int -> IO B.ByteString) -> String -> Benchmark
+benchmarkRNG rng name = bench name (whnfIO (liftM B.unpack (rng (2^27))))
