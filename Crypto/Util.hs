e7f8a9e111c6d82b31c2723f1f55ee4d144e0dfb
thomas.dubuisson <thomas.dubuisson@gmail.com>
2011-05-05 13:30:03 -0700

    Add missing files
    Ignore-this: d76766f74de38816f059984057b25797
    
    darcs-hash:20110505203003-cef97-d04d5d3e7376df854d529f670d27945c12c7db98.gz

---
+++ Crypto/Util.hs
@@ -0,0 +1,26 @@
+module Crypto.Util where
+import qualified Data.ByteString as B
+import Data.ByteString.Unsafe (unsafeIndex)
+import Data.Bits (shiftL, shiftR)
+
+-- |@incBS bs@ inefficiently computes the value @i2bs (8 * B.length bs) (bs2i bs + 1)@
+incBS :: B.ByteString -> B.ByteString
+incBS bs = B.concat (go bs (B.length bs - 1))
+  where
+  go bs i
+        | B.length bs == 0     = []
+        | unsafeIndex bs i == 0xFF = (go (B.init bs) (i-1)) ++ [B.singleton 0]
+        | otherwise            = [B.init bs] ++ [B.singleton $ (unsafeIndex bs i) + 1]
+{-# INLINE incBS #-}
+
+
+-- |@i2bs bitLen i@ converts @i@ to a 'ByteString' of @bitLen@ bits (must be a multiple of 8).
+i2bs :: Int -> Integer -> B.ByteString
+i2bs l i = B.unfoldr (\l' -> if l' < 0 then Nothing else Just (fromIntegral (i `shiftR` l'), l' - 8)) (l-8)
+{-# INLINE i2bs #-}
+
+-- |@bs2i bs@ converts the 'ByteString' @bs@ to an 'Integer' (inverse of 'i2bs')
+bs2i :: B.ByteString -> Integer
+bs2i bs = B.foldl' (\i b -> (i `shiftL` 8) + fromIntegral b) 0 bs
+{-# INLINE bs2i #-}
+
